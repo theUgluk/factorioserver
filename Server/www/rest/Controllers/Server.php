bf77817daf3fcc2b488f0f882dd8282078c1b1c4
Ugluk <jonasmuilwijk@gmail.com>
2020-06-20 22:43:36 +0200

    Testing

--- Server/www/rest/Controllers/Server.php
+++ Server/www/rest/Controllers/Server.php
@@ -3,7 +3,6 @@
 class Controllers_Server extends RestController {
     public function get(){
       var_dump("test");
-      
       exit();
     }
 
--- Server/www/rest/index.php
+++ Server/www/rest/index.php
@@ -1,4 +1,5 @@
 <?php
+exit("Test");
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 // Define path to application directory
