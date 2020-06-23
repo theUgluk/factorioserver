2e9592dac79d4a6c812b651b99cabe798da16955
Ugluk <jonasmuilwijk@gmail.com>
2020-06-23 17:55:19 +0200

    REST updates
    Created ScriptHelper for activating Bash scripts, wrote save code in REST API

--- Server/www/rest/Controllers/Server.php
+++ Server/www/rest/Controllers/Server.php
@@ -1,11 +1,21 @@
 <?php
 
 class Controllers_Server extends RestController {
-    public function get(){
-      if(isset($_GET['action']) && !empty($_GET['action'])){
+
+  private $scriptHelper;
+  private $mapArray;
+
+  public function __construct($request) {
+    $this->scriptHelper = new Tools_ScriptHelper();
+    $this->mapArray = $this->loadSaves();
+    parent::__construct();
+  }
+
+  public function get(){
+    if(isset($_GET['action']) && !empty($_GET['action'])){
         switch($_GET['action']){
           case "mapArray":
-            $this->response = json_decode(file_get_contents("/var/www/rest/mapArray.json"));
+            $this->response = $this->mapArray;
             $this->responseStatus = 200;
             return true;
           break;
@@ -14,13 +24,27 @@ class Controllers_Server extends RestController {
     }
 
     public function post(){
-
+      if(isset($_GET['action']) && !empty($_GET['action'])){
+          switch($_GET['action']){
+            case "changeSave":
+              if(array_key_exists("map", $this->params){
+                $this->scriptHelper->runScript("changeSave", $this->params->map);
+                $this->response = array();
+                $this->responseStatus = 200;
+              }
+            break;
+          }
+      }
     }
     public function put(){
 
     }
     public function delete(){
 
+    }
+
+    private function loadSaves(){
+      return json_decode(file_get_contents("/var/www/rest/mapArray.json"));
     }
 
 }
---
+++ Server/www/rest/Tools/ScriptHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+  class Tools_ScriptHerlper {
+    private $allowedScripts = array(
+        "changeSave" => "/var/www/rest/changeSave.sh",
+        "createBackup" => "/opt/backups/scripts/makeBackup.sh"
+    );
+    public function runScript($scriptName, $parameters){
+      if(array_key_exists($scriptName)){
+        $returnArray = array();
+        exec($this->allowedScripts[$scriptName] . " " . $parameters, $returnArray);
+        return $returnArray;
+      }
+      return false;
+    }
+  }
+
+?>
