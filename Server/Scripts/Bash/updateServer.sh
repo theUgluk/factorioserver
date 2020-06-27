9e5944b87f254e425e89f59d9d55df855f574046
Ugluk <jonasmuilwijk@gmail.com>
2020-06-27 16:08:52 +0200

    Terting with dry runs

--- Server/Scripts/Bash/updateServer.sh
+++ Server/Scripts/Bash/updateServer.sh
@@ -3,7 +3,7 @@
 #Test if there are updates available
 testResult=$(python3 /opt/factorio/update_factorio.py -xd --apply-to /opt/factorio/bin/x64/factorio)
 
-echo testResult
+echo $testResult
 
 #"No updates available for version"
 #sudo python3 update_factorio.py -xD --apply-to /opt/factorio/bin/x64/factorio && chown -R factorio:factorio /opt/factorio/
