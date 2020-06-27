9a6567094af1ef71ef00936e3e9803783c8fac40
Ugluk <jonasmuilwijk@gmail.com>
2020-06-27 16:27:26 +0200

    Actually preforming the update

--- Server/Scripts/Bash/updateServer.sh
+++ Server/Scripts/Bash/updateServer.sh
@@ -4,14 +4,14 @@
 testResult=$(python3 /opt/factorio/update_factorio.py -xd --apply-to /opt/factorio/bin/x64/factorio)
 
 #"No updates available for version"
-#sudo python3 update_factorio.py -xD --apply-to /opt/factorio/bin/x64/factorio && chown -R factorio:factorio /opt/factorio/
+# && chown -R factorio:factorio /opt/factorio/
 if [[ $testResult == *"would have fetched update"* ]]; then
-  #Create backup
+  # Create backup
   saveDate=`date '+%Y.%m.%d.%H.%M'`
-  echo $saveDate
   mkdir "/opt/backups/folders/$saveDate/"
   cp -R /opt/factorio/* /opt/backups/folders/$saveDate/
-#  echo "Update!"
+  # Do the actual update
+  python3 update_factorio.py -xD --apply-to /opt/factorio/bin/x64/factorio
 else
   echo "No update!"
 fi
