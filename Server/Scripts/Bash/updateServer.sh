59bf07c75b09825052da8c22831a3c9fff07622b
Ugluk <jonasmuilwijk@gmail.com>
2020-06-27 16:23:52 +0200

    More debugging

--- Server/Scripts/Bash/updateServer.sh
+++ Server/Scripts/Bash/updateServer.sh
@@ -9,9 +9,9 @@ if [[ $testResult == *"would have fetched update"* ]]; then
   #Create backup
   saveDate=`date '+%Y.%m.%d.%H.%M'`
   echo $saveDate
-  mkdir /opt/backups/folders/$saveDate/
+  mkdir "/opt/backups/folders/$saveDate/"
 #  cp -R /opt/factorio/* /opt/backups/folders/$saveDate/
 #  echo "Update!"
 else
-#  echo "No update!"
+  echo "No update!"
 fi
