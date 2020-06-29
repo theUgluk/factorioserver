cd41b976cae4925fc427ae87a4250830333db0ea
jmuilwijk <muilwijk@cardsolutions.nl>
2020-06-29 16:00:43 +0200

    Testen

--- Client/index.php
+++ Client/index.php
@@ -1,12 +1,6 @@
 <?php
 require("restHelper.php");
 
-//var_dump(json_decode(file_get_contents("http://77.160.106.150:81/rest/Server?action=mapArray")));
-
-$handle = fopen("http://77.160.106.150:81/rest/Server?action=mapArray", "r");
-var_dump($handle);
-$stats = fstat($handle);
-var_dump($stats);
-$contents = fread($handle, $stats['size']);
+var_dump(json_decode(file_get_contents("http://77.160.106.150:81/rest/Server?action=mapArray")));
 
 // var_dump(json_decode(CallAPI("GET", "http://77.160.106.150:81/rest/Server?action=mapArray"), true));
