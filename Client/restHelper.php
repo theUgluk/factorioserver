066ba614c2a4f5b1369da8b3d045b2707ed308de
jmuilwijk <muilwijk@cardsolutions.nl>
2020-06-29 13:06:10 +0200

    Added curl emulator

--- Client/restHelper.php
+++ Client/restHelper.php
@@ -1,21 +1,22 @@
 <?php
 ini_set("display_errors", 1);
+require_once("curlEmulator.php");
 // Method: POST, PUT, GET etc
 // Data: array("param" => "value") ==> index.php?param=value
 //Directly copied from SO, cuz I'm still a dev (https://stackoverflow.com/questions/9802788/call-a-rest-api-in-php)
 function CallAPI($method, $url, $data = false)
 {
-    $curl = curl_init();
+    $curl = curlemu_init();
 
     switch ($method){
         case "POST":
-            curl_setopt($curl, CURLOPT_POST, 1);
+            curlemu_setopt($curl, CURLOPT_POST, 1);
             if ($data){
-              curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+              curlemu_setopt($curl, CURLOPT_POSTFIELDS, $data);
             }
             break;
         case "PUT":
-            curl_setopt($curl, CURLOPT_PUT, 1);
+            curlemu_setopt($curl, CURLOPT_PUT, 1);
             break;
         default:
             if ($data)
@@ -23,16 +24,16 @@ function CallAPI($method, $url, $data = false)
     }
 
     // Optional Authentication:
-    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($curl, CURLOPT_PORT, 81);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curlemu_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curlemu_setopt($curl, CURLOPT_PORT, 81);
+    curlemu_setopt($curl, CURLOPT_URL, $url);
+    curlemu_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-    $result = curl_exec($curl);
+    $result = curlemu_exec($curl);
 
     if(!$result){
-      var_dump(curl_error($curl));
+      var_dump(curlemu_error($curl));
     }
-    curl_close($curl);
+    curlemu_close($curl);
     return $result;
 }
