c92bfd66a2d7c100e2e0c98b3bcf3c4dbf959255
jmuilwijk <muilwijk@cardsolutions.nl>
2020-06-29 17:21:18 +0200

    Set up basis for server management

--- Client/index.php
+++ Client/index.php
@@ -1,6 +1,36 @@
+<!doctype html>
 <?php
-require("restHelper.php");
+require("Helpers/restHelper.php");
 
-var_dump(json_decode(file_get_contents("http://77.160.106.150:81/rest/Server?action=mapArray")));
-
-// var_dump(json_decode(CallAPI("GET", "http://77.160.106.150:81/rest/Server?action=mapArray"), true));
+$saves = json_decode(file_get_contents("http://77.160.106.150:81/rest/Server?action=mapArray"), true)["saves"];
+?>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.21/datatables.min.css"/>
+    <title>
+      Factorio manager
+    </title>
+  </head>
+  <body>
+    <table id="saveOverview">
+      <thead>
+        <td>
+          Save name
+        </td>
+        <td>
+          Actions
+        </td>
+      </thead>
+      <tbody>
+        <?php
+          foreach($saves as $save){
+            echo "<tr><td>" . $save . "</td><td></td></tr>";
+          }
+        ?>
+    </table>
+  </body>
+</html>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jq-3.3.1/dt-1.10.21/datatables.min.js"></script>
+<script>
+  $("#saveOverview").DataTable();
+</script>
